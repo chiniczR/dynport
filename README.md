33ded5bf6f02bbbb853eb49621d684fd49b0aaaf
chiniczr <chiniczrebeca@gmail.com>
2020-12-14 20:18:55 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
 # Dynport
 
-Simple python utility that uses the Pandas library to help in directly importing data from a file to Amazon DynamoDB
+Simple python utility that uses the Pandas library to assist in directly importing data from a file to Amazon DynamoDB.
 
-WORK IN PROGRESS - 
-Currently, only imports a CSV, Excel, JSON or Parquet file.
+It can import a CSV, Excel, JSON or Parquet file.
 
-Datetimes are imported as strings.
+**Important observation: Datetimes are imported as strings.**
+
+### Installation
+With pip (replace with pip3 on Ubuntu)
+```
+pip install -r requirement.txt
+```
+With Pipenv
+```
+pipenv install
+```
+
+### Usage
+```
+dynport.py [-h] [--ftype [FILE-TYPE]] FILE AWS-ACCESS-KEY AWS-SECRET-KEY AWS-REGION TABLE-NAME
+```
+
+### Examples
+
+- **CSV**
+
+1. Create a DynamoDB table called Grades, in the US West 1 region, with primary key **_SSN_**, of string type
+
+> *FOR MORE INFO VISIT: [Getting Started with DynamoDB - Step 1: Create a Table](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/getting-started-step-1.html)*
+
+2. Use dynport to import the data in [grades.csv](./test-data/grades.csv)
+```
+dynport.py ./test-data/grades.csv <AWS-ACCESS-KEY> <AWS-SECRET-KEY> us-west-1 Users
+```
+
+- **Parquet**
+
+1. Create a DynamoDB table called Users, in the US West 1 region, with primary key **_id_**, of numeric type
+
+2. Use dynport to import the data in [userdata1.parquet](./test-data/userdata1.parquet)
+```
+dynport.py --ftype parquet ./test-data/userdata1.parquet <AWS-ACCESS-KEY> <AWS-SECRET-KEY> us-west-1 Users
+```
+
+For testing purposes, a sample of the other accepted file types can also be found in [test-data](./test-data)
--- dynport.py
+++ dynport.py
@@ -10,83 +10,86 @@ import sys
 from tqdm import tqdm 
 from decimal import Decimal
 
-# Parse the necessary arguments
-parser = argparse.ArgumentParser(prog='dynport', description='Import data from file to DynamoDB.')
-parser.add_argument('infile', metavar='FILE',  type=str, nargs=1,
-    help='file with the data to be imported')
-parser.add_argument('key', metavar='AWS-ACCESS-KEY', type=str, nargs=1,
-    help='aws access key')
-parser.add_argument('secret', metavar='AWS-SECRET-KEY', type=str, nargs=1,
-    help='aws secret access key')
-parser.add_argument('region', metavar='AWS-REGION', type=str, nargs=1,
-    help='aws region')
-parser.add_argument('table', metavar='TABLE-NAME', type=str, nargs=1,
-    help='the destination table')
-parser.add_argument('--ftype', metavar='FILE-TYPE', required=False, default='csv', choices=['csv', 'json', 'excel', 'orc', 'parquet', 'avro' ], type=str, nargs='?',
-    help='type of the file to be imported')
+def main():
+    # Parse the necessary arguments
+    parser = argparse.ArgumentParser(prog='dynport', description='Import data from file to DynamoDB.')
+    parser.add_argument('infile', metavar='FILE',  type=str, nargs=1,
+        help='file with the data to be imported')
+    parser.add_argument('key', metavar='AWS-ACCESS-KEY', type=str, nargs=1,
+        help='aws access key')
+    parser.add_argument('secret', metavar='AWS-SECRET-KEY', type=str, nargs=1,
+        help='aws secret access key')
+    parser.add_argument('region', metavar='AWS-REGION', type=str, nargs=1,
+        help='aws region')
+    parser.add_argument('table', metavar='TABLE-NAME', type=str, nargs=1,
+        help='the destination table')
+    parser.add_argument('--ftype', metavar='FILE-TYPE', required=False, default='csv', choices=['csv', 'json', 'excel', 'orc', 'parquet', 'avro' ], type=str, nargs='?',
+        help='type of the file to be imported')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# Import data
-dataset = None
+    # Import data
+    dataset = None
 
-if args.ftype == 'csv':
+    if args.ftype == 'csv':
+        try:
+            dataset = pd.read_csv(args.infile[0], encoding='utf-8')
+        except:
+            sys.exit(f'Error: {sys.exc_info()[1]}')
+    elif args.ftype == 'excel':
+        try:
+            dataset = pd.read_excel(args.infile[0])
+        except:
+            sys.exit(f'Error: {sys.exc_info()[1]}')
+    elif args.ftype == 'json':
+        try:
+            dataset = pd.read_json(args.infile[0], encoding='utf-8')
+        except:
+            sys.exit(f'Error: {sys.exc_info()[1]}')
+    elif args.ftype == 'parquet':
+        try:
+            dataset = pd.read_parquet(args.infile[0])
+        except:
+            sys.exit(f'Error: {sys.exc_info()[1]}')
+
     try:
-        dataset = pd.read_csv(args.infile[0], encoding='utf-8')
-    except:
-        sys.exit(f'Error: {sys.exc_info()[1]}')
-elif args.ftype == 'excel':
-    try:
-        dataset = pd.read_excel(args.infile[0])
-    except:
-        sys.exit(f'Error: {sys.exc_info()[1]}')
-elif args.ftype == 'json':
-    try:
-        dataset = pd.read_json(args.infile[0], encoding='utf-8')
-    except:
-        sys.exit(f'Error: {sys.exc_info()[1]}')
-elif args.ftype == 'parquet':
-    try:
-        dataset = pd.read_parquet(args.infile[0])
-    except:
-        sys.exit(f'Error: {sys.exc_info()[1]}')
-
-try:
-    dataset = dataset.rename(columns=lambda x: x.strip()) # Trim whitespace from column names
-    for col in dataset.columns:
-        if 'datetime' in str(dataset[col].dtype):   # Convert datetime types to string
-            dataset[col] = dataset[col].astype(str)
-        elif 'float' in str(dataset[col].dtype):    # Cast floats as Decimals
-            dataset[col].fillna(0, inplace=True)
-            dataset[col] = dataset[col].apply(lambda x: Decimal(str(x)))
-except:
-    sys.exit(f'Error: {sys.exc_info()[1]}')
-
-print('File has been read. Going to import:')
-
-dataset = dataset.T.to_dict().values()
-
-# Setting up the AWS credentials
-MY_ACCESS_KEY_ID = args.key[0]
-MY_SECRET_ACCESS_KEY = args.secret[0]
-MY_REGION = args.region[0]
-
-# The table we want the data imported to
-table_name = args.table[0]
-
-# The DynamoDB client and the table in question
-try:
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id=MY_ACCESS_KEY_ID, aws_secret_access_key=MY_SECRET_ACCESS_KEY, region_name=MY_REGION)
-    table = dynamodb.Table(table_name)
-except:
-    sys.exit(f'Error: {sys.exc_info()[1]}')
-
-# For each item on the file, put it on the table
-for item in tqdm (dataset, desc="Putting items"):
-    try:
-        table.put_item(Item=item)
+        dataset = dataset.rename(columns=lambda x: x.strip()) # Trim whitespace from column names
+        for col in dataset.columns:
+            if 'datetime' in str(dataset[col].dtype):   # Convert datetime types to string
+                dataset[col] = dataset[col].astype(str)
+            elif 'float' in str(dataset[col].dtype):    # Cast floats as Decimals
+                dataset[col].fillna(0, inplace=True)
+                dataset[col] = dataset[col].apply(lambda x: Decimal(str(x)))
     except:
         sys.exit(f'Error: {sys.exc_info()[1]}')
 
-print('Complete')
+    print('File has been read. Going to import:')
 
+    dataset = dataset.T.to_dict().values()
+
+    # Setting up the AWS credentials
+    MY_ACCESS_KEY_ID = args.key[0]
+    MY_SECRET_ACCESS_KEY = args.secret[0]
+    MY_REGION = args.region[0]
+
+    # The table we want the data imported to
+    table_name = args.table[0]
+
+    # The DynamoDB client and the table in question
+    try:
+        dynamodb = boto3.resource('dynamodb', aws_access_key_id=MY_ACCESS_KEY_ID, aws_secret_access_key=MY_SECRET_ACCESS_KEY, region_name=MY_REGION)
+        table = dynamodb.Table(table_name)
+    except:
+        sys.exit(f'Error: {sys.exc_info()[1]}')
+
+    # For each item on the file, put it on the table
+    for item in tqdm (dataset, desc="Putting items"):
+        try:
+            table.put_item(Item=item)
+        except:
+            sys.exit(f'Error: {sys.exc_info()[1]}')
+
+    print('Complete')
+
+if __name__ == "__main__":
+    main()
